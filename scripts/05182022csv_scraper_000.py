c38a22c8348b32af96ddeb37d153b7db98602004
pyromakesmusic <tayo.castro@gmail.com>
2022-05-20 01:33:11 -0500

    Broke it, but got a different error in the DataFrame conversion

--- scripts/05182022csv_scraper_000.py
+++ scripts/05182022csv_scraper_000.py
@@ -7,7 +7,7 @@ import os
 import time
 import numpy as np
 import csv
-import pandas
+import pandas as pd
 
 safe_sites = ['C1_2','C8_2','C15_3','C26_2','C35_1','C45_1','C53_1','C78_1','C84_1','C403_3','C405_1','C406_1','C408_2','C409_2','C410_1','C416_1','C603_1','C603_2','C603_3','C617_1','C620_1','C1015_1','C1016_1','C1034_1']
 
@@ -33,15 +33,29 @@ with open('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\ozone_isoheight_2010-
     
 # Now we have printed the 1D array, we want to use numpy functions to turn the 1d array of lists into a 2d array.
     ozone_2d = np.array(ozone_array)
+    ozone_df = pd.DataFrame(data=ozone_2d, columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49])
+    print(ozone_df)
  
 #    for i in ozone_2d:
 #       print(ozone_2d[i])
-    print(ozone_2d)
+#   print(ozone_2d)
     
-    for i in range(len(ozone_2d)):
-        print(ozone_2d[i])
-        
-        
+#   for i in range(len(ozone_2d)):
+#       print(ozone_2d[i])
+    
+ 
+"""
+ Will need to use modular arithmetic for this next bit.
+ Modulus is either 48, 49, or 50. Number of rows in each
+ copy-paste of data. So when our index modulo that value
+ is certain numbers, we want the row to be stricken from the record.
+"""
+"""
+for i in range(3):
+    print(ozone_2d[i][0])
+"""
+
+     
 """
 We need to find ways to identify and label the different sets of cells.
 A few possible identifiers for cells: month, list of headings, site title, instrument identifier, regulated or non-regulatory, blank, dates, max daily ozone.
