90e7e74ac9268478290be743cf44c2b42320f530
pyromakesmusic <tayo.castro@gmail.com>
2022-05-25 04:51:34 -0500

    reorganized a little bit. global vars are in one place, function defs are in one place, function calls in another.

--- scripts/05182022csv_scraper_000.py
+++ scripts/05182022csv_scraper_000.py
@@ -89,44 +89,14 @@ rows_good = []
 safesite_final = np.asarray(safe_siteID)  
 ozone_dflist_good = [[]]
 multi_ozone_index = pd.MultiIndex.from_product([sites_good, month_date])
+final_df = [[]] 
+# I think some of these variables are essentially duplicates and can be eliminated. We will see.
 
-def sitename_strip():
-    i = 0
-    for i in range(0, len(safe_sites)):
-        safe_siteID.append(i)
-        safe_sitestrp = safe_sites[i].partition('_')
-        safe_siteID[i] = safe_sitestrp
-        i = i + 1 
-       
-        
-def montharray_slicer():
-    i = 0
-    while i < len(slice_locations):
-#        print(ozone_df[0][slice_locations[i]])
-        i = i + 1
-
-def month_dfinput():
-    i = 0
-    while i < len(month_date):
-        ozone_dflist_good.append(month_date[i])
-        i = i + 1
-    print(ozone_dflist_good)
-#    print(ozone_dflist_good[0][0])
-
-# print(month_date)
-print(len(month_date))
-month_dfinput()
-print(ozone_df.iloc(slice_locations))
-# Some function calls for testing.
-        
-#print(safe_siteID)
-#print(len(safe_sites))
-#print(len(safe_siteID))
-        
-"""
 We need to figure out what the heck this function does. I think it checks if the current row intersects with the good sites.
 """
 
+
+"""
 def loc_slice(x):
     i = 0
     current_row = []
@@ -143,16 +113,24 @@ def loc_slice(x):
         else:
             row_intersect = []
             i = i + 1
+"""
 
+
+"""
 def sitestrp():
     i = 0
     while i < len(safesite_final):
         sites_good.append(safesite_final[i, 0])
         instr_good.append(safesite_final[i, 2])
         i = i + 1    
-"""        
+""" 
+
+
+"""       
         print(sites_good)
         print(instr_good)        
+"""
+
 """
 
 
@@ -188,6 +166,128 @@ def rowcount():
             print(i)
             i = i + 1
 #        print(slice_index)
+            
+# May 25 0418 AM this is currently where i am at
+   
+"""    
+"""        
+def year_mod12(x):
+    return x % 12
+"""
+
+
+"""
+def df_create():
+    i = 1 # i is  the row counter
+    year_counter = 2010
+    month_counter = 0
+    while i < 5337:
+        
+        if ozone_df[0][i][0] == month_date[month_counter]:
+            print(ozone_df[0][i][0])
+            final_df[year_counter][i].append(ozone_df[month_counter])
+            if month_counter % 12 == 0:
+                year_counter = year_counter + 1
+                i = i + 1    
+            else:
+                    i = i + 1
+    else:
+        print('This is a test.')
+        i = i + 1
+        
+        
+def index_check():
+    while True:
+        try:
+            first = int(input('What is the first index of your desired cell? '))
+            break
+        except:
+            print('That is invalid input.')
+    while True:
+        try:
+            second = int(input('What is the second index of your desired cell? '))
+            break
+        except:
+            print('That is invalid input.')
+    
+#   print(ozone_2d[first][second])
+            
+            
+def row_scraper(x,p,q,r):  
+#    print(month_date)
+    print(ozone_df[0][0])
+    row_intersect = []
+    month_counter = 0
+    year_counter = 2010
+    ozone_df_good = []
+    
+    i = 0
+    while i < x:
+        if np.intersect1d(ozone_df[0][i][0], month_date) == number:
+            row_intersect.append(np.intersect1d(ozone_df[0][i], month_date[month_counter]))
+            month_counter = month_counter + 1
+            if month_counter % 12 == 6:
+                year_counter == year_counter + 1
+                i = i + 1
+            else:
+                i = i + 1
+        elif np.intersect1d(ozone_df[0][i][q], site_good) == number and np.intersect1d(ozone_df[0][i][r], instr_good) == number:
+            i = i + 1
+        else:
+            i = i + 1
+            
+def site_pop():
+    i = 1
+    while i < len(safe_sites) + 1:
+            if i == safe_sites.index(safe_sites[i]):
+                safe_siteID.append(safe_sites[i])
+            else:
+                i = i + 1
+    print(safe_sites)  
+    
+    
+# Okay, now we want a function where the years go up vertically from 2010 to 2019, the months and days all lie along the y axis, and the x axis is max daily 8-hour ozone.
+
+
+# This is not currently needed.
+
+
+"""
+def make_plot():
+    x = np.linspace(start=1, stop=12,num=12)
+    y = np.linspace(start=2010, stop=2019, num=12)
+    z = np.linspace(start=-10, stop=150, num=15)
+    fig = mpl.pyplot.figure()
+    pyplot_2 = fig.plot3D(x,y,z, projection='3D')
+
+    plt.axes(projection='3d')
+
+
+    x = np.linspace(0, 2 *np.pi, 200)
+    y = np.sin(x)
+    fig, ax = plt.subplots()
+    ax.plot(x,y)
+    plt.show()
+    
+
+site_pop()
+   
+
+row_scraper(800,0,1,2)
+    
+df_create()
+"""
+
+
+# print(month_date)
+# print(len(month_date))
+# month_dfinput()
+#print(ozone_dflist_good)
+# Some function calls for testing.
+        
+#print(safe_siteID)
+#print(len(safe_sites))
+#print(len(safe_siteID))
 
 
 # Don't forget to call the fucking function!!!
@@ -222,165 +322,13 @@ def rowcount():
 
 
 
-"""
-print(ozone_df[0][0])
-print(ozone_df[0][49])
-print(ozone_df[0][98])
-print(ozone_df[0][148])
-print(ozone_df[0][199])
-print(ozone_df[0][250])
-
-print(ozone_df[0][301])
-print(ozone_df[0][352])
-print(ozone_df[0][403])
-print(ozone_df[0][454])
-print(ozone_df[0][505])
-print(ozone_df[0][556])
-print(ozone_df[0][607])
-print(ozone_df[0][658])
-print(ozone_df[0][708])
-print(ozone_df[0][758])
-print(ozone_df[0][809])
-print(ozone_df[0][858])
-
-print(ozone_df[0][907])
-print(ozone_df[0][957])
-print(ozone_df[0][1008])
-print(ozone_df[0][1059])
-print(ozone_df[0][1110])
-print(ozone_df[0][1160])
-print(ozone_df[0][1210])
-print(ozone_df[0][1261])
-print(ozone_df[0][1311])
-print(ozone_df[0][1361])
-print(ozone_df[0][1409])
-print(ozone_df[0][1457])
-
-print(ozone_df[0][1505])
-print(ozone_df[0][1553])
-print(ozone_df[0][1601])
-print(ozone_df[0][1649])
-print(ozone_df[0][1697])
-print(ozone_df[0][1743])
-print(ozone_df[0][1789])
-print(ozone_df[0][1836])
-print(ozone_df[0][1883])
-print(ozone_df[0][1930])
-print(ozone_df[0][1978])
-print(ozone_df[0][2025])
-print(ozone_df[0][2072])
-
-print(ozone_df[0][2119])
-print(ozone_df[0][2166])
-print(ozone_df[0][2213])
-print(ozone_df[0][2261])
-print(ozone_df[0][2309])
-print(ozone_df[0][2357])
-print(ozone_df[0][2405])
-print(ozone_df[0][2456])
-print(ozone_df[0][2507])
-print(ozone_df[0][2558])
-print(ozone_df[0][2609])
-print(ozone_df[0][2659])
-
-print(ozone_df[0][2710])
-print(ozone_df[0][2761])
-print(ozone_df[0][2812])
-print(ozone_df[0][2863])
-print(ozone_df[0][2914])
-print(ozone_df[0][2965])
-print(ozone_df[0][3016])
-print(ozone_df[0][3067])
-print(ozone_df[0][3118])
-print(ozone_df[0][3169])
-print(ozone_df[0][3219])
-print(ozone_df[0][3270])
-print(ozone_df[0][3321])
-
-print(ozone_df[0][3371])
-print(ozone_df[0][3421])
-print(ozone_df[0][3471])
-print(ozone_df[0][3522])
-print(ozone_df[0][3573])
-print(ozone_df[0][3623])
-print(ozone_df[0][3672])
-print(ozone_df[0][3722])
-print(ozone_df[0][3772])
-print(ozone_df[0][3820])
-print(ozone_df[0][3866])
-
-print(ozone_df[0][3913])
-print(ozone_df[0][3960])
-print(ozone_df[0][4005])
-print(ozone_df[0][4053])
-print(ozone_df[0][4099])
-print(ozone_df[0][4145])
-print(ozone_df[0][4190])
-print(ozone_df[0][4235])
-print(ozone_df[0][4280])
-print(ozone_df[0][4324])
-print(ozone_df[0][4369])
-print(ozone_df[0][4412])
-print(ozone_df[0][4456])
-
-print(ozone_df[0][4495])
-print(ozone_df[0][4536])
-print(ozone_df[0][4579])
-print(ozone_df[0][4622])
-print(ozone_df[0][4665])
-print(ozone_df[0][4707])
-print(ozone_df[0][4750])
-print(ozone_df[0][4793])
-print(ozone_df[0][4836])
-print(ozone_df[0][4879])
-print(ozone_df[0][4920])
-print(ozone_df[0][4959])
-
-print(ozone_df[0][4998])
-print(ozone_df[0][5036])
-print(ozone_df[0][5075])
-print(ozone_df[0][5118])
-print(ozone_df[0][5160])
-print(ozone_df[0][5203])
-print(ozone_df[0][5246])
-print(ozone_df[0][5292])
-"""
-
-
-
 
 
 
 #print(multi_ozone_index[1][1])
     
 """
-def row_scraper(x,p,q,r):  
-#    print(month_date)
-    print(ozone_df[0][0])
-    row_intersect = []
-    month_counter = 0
-    year_counter = 2010
-    ozone_df_good = []
-    
-    i = 0
-    while i < x:
-        if np.intersect1d(ozone_df[0][i][0], month_date) == number:
-            row_intersect.append(np.intersect1d(ozone_df[0][i], month_date[month_counter]))
-            month_counter = month_counter + 1
-            if month_counter % 12 == 6:
-                year_counter == year_counter + 1
-                i = i + 1
-            else:
-                i = i + 1
-        elif np.intersect1d(ozone_df[0][i][q], site_good) == number and np.intersect1d(ozone_df[0][i][r], instr_good) == number:
-            i = i + 1
-        else:
-            i = i + 1
-            
 
-   
-
-row_scraper(800,0,1,2)
 """
      
 # What if this worked....
@@ -451,16 +399,7 @@ while i < 5336:
 # This next function is garbage. Zero points.
 
 """
-def site_pop():
-    i = 1
-    while i < len(safe_sites) + 1:
-            if i == safe_sites.index(safe_sites[i]):
-                safe_siteID.append(safe_sites[i])
-            else:
-                i = i + 1
-    print(safe_sites)  
-    
-site_pop()
+
 
 
 
@@ -500,28 +439,7 @@ for month in month_date:
 #    break
 
 
-# Okay, now we want a function where the years go up vertically from 2010 to 2019, the months and days all lie along the y axis, and the x axis is max daily 8-hour ozone.
 
-
-# This is not currently needed.
-
-
-"""
-def make_plot():
-    x = np.linspace(start=1, stop=12,num=12)
-    y = np.linspace(start=2010, stop=2019, num=12)
-    z = np.linspace(start=-10, stop=150, num=15)
-    fig = mpl.pyplot.figure()
-    pyplot_2 = fig.plot3D(x,y,z, projection='3D')
-
-    plt.axes(projection='3d')
-
-
-    x = np.linspace(0, 2 *np.pi, 200)
-    y = np.sin(x)
-    fig, ax = plt.subplots()
-    ax.plot(x,y)
-    plt.show()
 """
 
 
@@ -537,15 +455,19 @@ def make_plot():
     
  
 """
- Will need to use modular arithmetic for this next bit.
- Modulus is either 48, 49, or 50. Number of rows in each
- copy-paste of data. So when our index modulo that value
- is certain numbers, we want the row to be stricken from the record.
+# Will need to use modular arithmetic for this next bit.
+# Modulus is either 48, 49, or 50. Number of rows in each
+# copy-paste of data. So when our index modulo that value
+# is certain numbers, we want the row to be stricken from the record.
+ 
+"""
 """
 
 # Commenting this out briefly, since I have roughly the same thing being defined in a function definition up top.
 
 """
+
+
 x = 0
 while x < ozone_df.count():
     print(ozone_df[i, 0])
@@ -555,24 +477,42 @@ while x < ozone_df.count():
 
 
 """
+
+
+
+"""
+"""
 We need to find ways to identify and label the different sets of cells.
 A few possible identifiers for cells: month, list of headings, site title, instrument identifier, regulated or non-regulatory, blank, dates, max daily ozone.
 """
+"""
+
+
 # Now let's give the user a chance to provide input in the form of  two integers which should be the index of some cell in the array so that we can attempt to find a pattern.
 """
-def index_check():
-    while True:
-        try:
-            first = int(input('What is the first index of your desired cell? '))
-            break
-        except:
-            print('That is invalid input.')
-    while True:
-        try:
-            second = int(input('What is the second index of your desired cell? '))
-            break
-        except:
-            print('That is invalid input.')
-    
-#   print(ozone_2d[first][second])
+
 """
+
+def sitename_strip():
+    i = 0
+    for i in range(0, len(safe_sites)):
+        safe_siteID.append(i)
+        safe_sitestrp = safe_sites[i].partition('_')
+        safe_siteID[i] = safe_sitestrp
+        i = i + 1 
+       
+        
+def montharray_slicer():
+    i = 0
+    while i < len(slice_locations):
+#        print(ozone_df[0][slice_locations[i]])
+        i = i + 1
+
+def month_dfinput():
+    i = 0
+    while i < len(month_date):
+        ozone_dflist_good.append(month_date[i])
+        i = i + 1
+#        print(ozone_dflist_good)
+    print(ozone_dflist_good)
+#    print(ozone_dflist_good[0][0])
