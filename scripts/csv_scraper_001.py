9525932908c276a1cf35593023552ea073d243df
pyromakesmusic <tayo.castro@gmail.com>
2022-05-28 21:28:40 -0500

    starting over and refactoring to make it smaller

---
+++ scripts/csv_scraper_001.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 28 21:26:20 2022
+
+@author: Ghost
+"""
+
+import os
+import time
+import datetime
+import math
+import string
+import numpy as np
+import csv
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+# Now we are starting to define some functions. The dependencies are all screwy here and some of this could probably be rearranged, but I'm mostly leaving it for now.
+
+def find_dir():
+    # Finds and navigates to the correct directory for the data.        
+    os.getcwd()
+    os.chdir('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\') 
+
+# Start of the use of the open file.
+with open('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\ozone_isoheight_2010-2020.txt', 'r') as inputfile:
+# Some global variables start to be defined          
+
+          
+# Now we are going to try to visualize some of the data, and clean it, with whichever comes fastest taking priority.
+    ozone_graph = [[]]
+
+#          ozone_array = np.loadtxt(inputfile, delimiter=",")
+    
+    inputfile_read = csv.reader(inputfile)
+# Trying this with pandas instead of csv now
+#    inputfile_read = pandas.read_csv(inputfile, sep=",")
+# Should load the input file in as an array
+    ozone_array = list(inputfile_read)
+
+    months_choices = []
+    for i in range(1,13):
+        months_choices.append((datetime.date(2009, i, i).strftime('%B')))
+
+    years_choices = []
+    for i in range(0,11):
+        years_choices.append(i)
+    
+    month_date = []
+    for i in range(5,117):
+        month_date.append(str(months_choices[i % 12] + ' ' + str(2010 + years_choices[math.floor(i * 1/12)])))                 
+
+#    print(ozone_array)
+    
+# Now we have printed the 1D array, we want to use numpy functions to turn the 1d array of lists into a 2d array.
+    ozone_2d = np.array(ozone_array)
+    ozone_2dnewrows = ozone_2d.reshape(5337,1)
+    ozone_df = pd.DataFrame(data=ozone_2dnewrows)
+
+# Sends the DataFrame to csv format
+def makecsv(name):
+    name.to_csv(str(name) + '.csv')
+    
+    
+safe_sites = ['C1_2','C8_2','C15_3','C26_2','C35_1','C45_1','C53_1','C78_1','C84_1','C403_3','C405_1','C406_1','C408_2','C409_2','C410_1','C416_1','C603_1','C603_2','C603_3','C617_1','C620_1','C1015_1','C1016_1','C1034_1']
+slice_locations = (0,49,98,148,199,250,301,352,403,454,505,607,658,708,758,809,
+                858,907,957,1008,1059,1110,1160,1210,1261,1311,1361,1409,1457,
+                1505,1553,1601,1649,1697,1743,1789,1836,1883,1930,1978,2025,
+                2072,2119,2166,2213,2261,2309,2357,2405,2456,2507,2558,2609,
+                2710,2761,2812,2863,2914,2965,3016,3067,3118,3169,3219,3270,
+                3321,3371,3421,3471,3522,3772,3820,3866,3913,3960,4005,4053,
+                4099,4145,4190,4235,4280,4324,4369,4412,4456,4495,4536,4579,
+                4622,4665,4707,4750,4793,4836,4879,4920,4959,4998,5036,5075,
+                5118,5160,5203,5246,5292)
+safe_siteID = []
+sites_good = []
+instr_good = []
+rows_good = []
+safesite_final = np.asarray(safe_siteID)  
+ozone_dflist_good = []
+multi_ozone_index = pd.MultiIndex.from_product([sites_good, month_date])
+final_df = [] 
