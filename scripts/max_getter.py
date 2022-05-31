532350520a39f9edae8376034db94ab6dea433a6
pyromakesmusic <tayo.castro@gmail.com>
2022-05-31 06:33:50 -0500

    started grabbing the dates from the SOM file

--- scripts/max_getter.py
+++ scripts/max_getter.py
@@ -29,7 +29,24 @@ from mpl_toolkits import mplot3d
 """
 2. Variable Declaration
 """
+daily_somdf = False
+some_fileread = False
+daily_som = False
 
+
+
+
+months_choices = []
+for i in range(1,13):
+    months_choices.append((datetime.date(2009, i, i).strftime('%B')))
+
+years_choices = []
+for i in range(0,11):
+    years_choices.append(i)
+        
+month_date = []
+for i in range(5,117):
+    month_date.append(str(months_choices[i % 12] + ' ' + str(2010 + years_choices[math.floor(i * 1/12)])))
 
 
 """
@@ -37,6 +54,16 @@ from mpl_toolkits import mplot3d
 """
 
 
+
+def get_day():
+    i = 1
+    while i < 1221:
+        current_date = daily_somdf[1][i]
+        print(current_date)
+        print(datetime.datetime.strptime(current_date, "%Y-%m-%d"))
+        i = i + 1
+    return(False)
+    
 
 """
 4. Main
@@ -46,14 +73,29 @@ from mpl_toolkits import mplot3d
 
 os.chdir('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\')
          
-with open('som_cluster_10yr_700hpa_00utc.csv') as som_file:
+with open('som_cluster_10yr_700hpa_00utc.csv') as som_file, open('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\cleaned_data.csv') as ozone_file:
               
     som_fileread = csv.reader(som_file)
-    print(som_fileread)  
     daily_som = list(som_fileread)
-    print(daily_som)
+    print(daily_som[0])
     daily_somdf = pd.DataFrame(data = daily_som)
+    
+    ozone_fileread = csv.reader(ozone_file)
+    daily_ozone = list(ozone_fileread)
+    daily_ozone.pop(0)
+    daily_ozonedf = pd.DataFrame(data = daily_ozone)
+    
+
+    
+    
 """
 5. Unit Tests
 """
-print(os.getcwd())
+
+print(len(daily_ozone[1]))
+
+get_day()
+
+"""
+Okay, now we know the dates for the SOM excel file appear in column index 1 and start at row 1.
+"""
