877de999c2d7a947f2095265a0e8975bc9a28b4f
pyromakesmusic <tayo.castro@gmail.com>
2022-05-31 03:54:36 -0500

    created the max_getter script file

---
+++ scripts/max_getter.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+==========
+MAX GETTER
+==========
+
+should take a .csv file, with a row for each month, and calculate the max ozone for each day at each data collection site.
+
+Created on Tue May 31 03:52:47 2022
+
+@author: Ghost
+"""
+
