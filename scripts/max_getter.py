84adf0ee043e2397795b4c5616a6ac88fd810f59
pyromakesmusic <tayo.castro@gmail.com>
2022-05-31 04:50:01 -0500

    renamed some vars to more accurately represent what they do. need to create two DataFrames; one from the SOM and one from the ozone analysis.

--- scripts/max_getter.py
+++ scripts/max_getter.py
@@ -26,3 +26,34 @@ import pandas as pd
 import matplotlib as mpl
 from mpl_toolkits import mplot3d
 
+"""
+2. Variable Declaration
+"""
+
+
+
+"""
+3. Function Definition
+"""
+
+
+
+"""
+4. Main
+
+"""
+# Navigating to the right folder
+
+os.chdir('D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\')
+         
+with open('som_cluster_10yr_700hpa_00utc.csv') as som_file:
+              
+    som_fileread = csv.reader(som_file)
+    print(som_fileread)  
+    daily_som = list(som_fileread)
+    print(daily_som)
+    daily_somdf = pd.DataFrame(data = daily_som)
+"""
+5. Unit Tests
+"""
+print(os.getcwd())
