6a85d82607a16c38437f1344e235713ab3a79a61
pyromakesmusic <tayo.castro@gmail.com>
2022-05-31 04:28:50 -0500

    imported a bunch of libraries to the max getter function.

--- scripts/max_getter.py
+++ scripts/max_getter.py
@@ -11,3 +11,18 @@ Created on Tue May 31 03:52:47 2022
 @author: Ghost
 """
 
+"""
+1. Libraries
+"""
+
+import os
+import time
+import datetime
+import math
+import string
+import numpy as np
+import csv
+import pandas as pd
+import matplotlib as mpl
+from mpl_toolkits import mplot3d
+
