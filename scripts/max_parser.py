7048b655a60b23af623292572e40118ee4e41d5f
pyromakesmusic <tayo.castro@gmail.com>
2022-06-13 08:14:15 -0500

    creating a new file to parse the output of the old one

---
+++ scripts/max_parser.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 08:12:18 2022
+
+@author: Ghost
+"""
+
+"""
+1.LIBRARIES
+"""
+
+"""
+2.GLOBAL VARIABLES
+"""
+
+"""
+3.FUNCTION DEFINITIONS
+"""
+
+"""
+4.MAIN
+"""
+
+"""
+5.UNIT TESTING
+"""
