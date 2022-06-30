712d68bbfda9fcfd713c857c10734460b52a5781
pyromakesmusic <tayo.castro@gmail.com>
2022-06-30 11:53:26 -0500

    once more into the breach. going to try with fresh clean data and building somewhat from the ground up, but using the stuff that worked before.

--- scripts/max_interpreter_3.py
+++ scripts/max_interpreter_3.py
@@ -5,3 +5,16 @@ Created on Thu Jun 30 11:18:18 2022
 @author: Ghost
 """
 
+import pandas as pd
+import numpy as np
+
+"""
+CONFIG
+"""
+
+filepath = "D:\#PERSONAL\#STEDWARDS\#Summer2022Research"
+
+"""
+FUNCTION DEFINITIONS
+"""
+
