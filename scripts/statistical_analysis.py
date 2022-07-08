bc11a3c5a12257edc8f3ee8829053fc02972f583
pyromakesmusic <tayo.castro@gmail.com>
2022-07-08 08:33:46 -0500

    made the first histogram

--- scripts/statistical_analysis.py
+++ scripts/statistical_analysis.py
@@ -11,5 +11,51 @@ import os
 import math
 import string
 import csv
-from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 import ast
+
+"""
+CONFIG
+"""
+source_filepath = "D:\\#PERSONAL\\#STEDWARDS\\#Summer2022Research\\SOM_cluster_augmented_2.csv"
+
+
+pd.options.display.width = 0
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+
+
+"""
+GLOBAL VARIABLES
+"""
+
+"""
+FUNCTION DEFINITIONS
+"""
+def histo_builder(df):
+    return(True)
+
+
+"""
+MAIN FUNCTION CALLS
+"""
+with open(source_filepath) as source:
+    data = pd.read_csv(source_filepath)
+    print("Hello World")
+    print(data)
+    data = data.iloc[: , :-1]
+    print(data)
+    data.drop(data.tail(1).index, inplace = True)
+    print(data)
+    
+    print(data.describe())
+    
+    plt.hist(data['max D8HO'])
+
+
+"""
+FILE OUTPUT
+"""
