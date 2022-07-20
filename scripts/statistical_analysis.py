9110bc61458cf9791a5edfbea1a92706056c5995
pyromakesmusic <tayo.castro@gmail.com>
2022-07-20 17:09:37 -0500

    made the bin widths uniform

--- scripts/statistical_analysis.py
+++ scripts/statistical_analysis.py
@@ -12,6 +12,7 @@ import math
 import string
 import csv
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import ast
 
 """
@@ -33,19 +34,25 @@ pd.set_option('display.max_colwidth', None)
 """
 GLOBAL VARIABLES
 """
+toplayer = mpl.figure.Figure()
 
 """
 FUNCTION DEFINITIONS
 """
-def histo_builder(df, x, y):
-    hist = df.hist(column="maximum", figsize=(6,5))
+def exceedance_counter():
+    # Takes something and returns number of exceedance days graphed on the screen
+    return(True)
+
+def histo_builder(df, x, y, figure):
+#    axes = mpl.axes.Axes(fig=figure, rect=[0,0,5,5], xlim=(0,120), ylim=(0,50))
+    hist = df.hist(column="maximum", figsize=(6,5), bins=[0,10,20,30,40,50,60,70,80,90,100,120], legend = True, xlabelsize=10, ylabelsize=10)
     return(hist)
 
 def cluster_plotter(df):
     i = 0
     while i < 16:
         cluster = df.loc[df['cluster'] == i]
-        histo_builder(cluster, "maximum daily 8 hour ozone (ppb)", "number of days")
+        histo_builder(cluster, "maximum daily 8 hour ozone (ppb)", "number of days", toplayer)
         i = i + 1
     return(True)
 
@@ -56,7 +63,7 @@ def year_overyearplotter(df):
     i = 2010
     while i < 2020:
         year = df.loc[df['year'] == i]
-        histo_builder(year, "maximum daily 8 hour ozone (ppb)", "number of days")
+        histo_builder(year, "maximum daily 8 hour ozone (ppb)", "number of days", toplayer)
         i = i + 1
     return(True)
 
@@ -64,7 +71,7 @@ def month_bymonthplotter(df):
     i = 6
     while i < 10:
         month = df.loc[df['month'] == i]
-        histo_builder(month, "maximum daily 8 hour ozone (ppb)", "number of days")
+        histo_builder(month, "maximum daily 8 hour ozone (ppb)", "number of days", toplayer)
         i = i + 1
     return(True)
 
@@ -93,6 +100,8 @@ print("now the months")
 print("and finally years")
 year_overyearplotter(data)
 
+
+"""
 with open(windrun_filepath) as windrun:
     windrun_data = pd.read_csv(windrun_filepath, delim_whitespace=True)
     print(windrun_data)
@@ -103,6 +112,14 @@ with open(moody_wind_filepath) as moody_wind:
     moody_data = pd.read_csv(moody_wind_filepath)
     print(moody_data)
     print(moody_data.describe())
+"""
+
+    
+"""    
+    plt.text(2, 4, "r2_cell", size=12, ha="center", va="center",
+    bbox=dict(boxstyle="round",  facecolor='blue', alpha=0.3) )
+"""
+
 """
 FILE OUTPUT
 """
