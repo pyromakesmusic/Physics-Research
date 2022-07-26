9e6402c9137bc56d3c736574eb1923280258de3d
pyromakesmusic <tayo.castro@gmail.com>
2022-07-26 04:24:29 -0500

    generated some examples for the year over year histograms

--- scripts/statistical_analysis.py
+++ scripts/statistical_analysis.py
@@ -22,7 +22,7 @@ CONFIG
 ozone_filepath = r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\SOM_cluster_AUGMENTED3_july19.csv"
 windrun_filepath = r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\data_210726.txt"
 moody_wind_filepath = r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\wind_data_2010_2019_MOOT_C695.csv"
-
+global_output_path = r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\scripts\file_outputs\yearoveryear_histograms"
 
 pd.options.display.width = 0
 pd.set_option('display.max_rows', None)
@@ -40,24 +40,7 @@ GLOBAL VARIABLES
 """
 FUNCTION DEFINITIONS
 """
-def histo_builder(df, place, holders):
-    # Takes a dataframe and returns a matplotlib histogram tuned to the output I want
-    fig, ax = plt.subplots(1,1, sharex=True,sharey=True)
-    hist = plt.hist(df["maximum"], bins=[0,10,20,30,40,50,60,70,80,90,100,110,120,130], histtype="barstacked", align="mid", rwidth=.85, label="maximum daily 8 hour ozone")
-    dropped = df.dropna(axis=0, how="any") # Think I need dropna or ge rather than gt
-    
-    plt.axvline(x=71, color="red", linestyle="dashed")
-    plt.title("Houston Area Ozone Levels", family="sans-serif")
-    plt.xlabel("Maximum Daily 8 Hour Ozone (ppb)", family="sans-serif")
-    plt.ylabel("Number of Days in Sample", family="sans=serif")
-    return(True)
 
-def time_separator(df, time_unit):
-    grouped = df.groupby(by=time_unit, as_index=True, sort=True, group_keys=True)
-    print(grouped)
-    print(len(grouped))
-    print(grouped.max())
-    return(True)
 
 def exceedance_counter(df, time_unit):
     # Takes a dataframe and period of time and returns number and percentage of exceedance days graphed on the screen, for that unit of time
@@ -72,6 +55,31 @@ def exceedance_counter(df, time_unit):
 #    print(masked)
 #    print(len(masked))
 #    print(masked["maximum"])
+   
+
+def histo_builder(df, unit, graph_id, print_flag, output_path, output_prefix):
+    # Takes a dataframe and returns a matplotlib histogram tuned to the output I want
+    fig, ax = plt.subplots(1,1, sharex=True,sharey=True)
+    bins = np.linspace(0,140,14)
+    plt.ylim(0, 50)
+    hist = plt.hist(df["maximum"], bins=bins, histtype="barstacked", align="left", rwidth=.85, label="maximum daily 8 hour ozone")
+    dropped = df.dropna(axis=0, how="any") # Think I need dropna or ge rather than gt
+    
+    plt.axvline(x=71, color="red", linestyle="dashed")
+    plt.title("Houston Area Ozone Levels by " + unit + ": " + str(graph_id), family="sans-serif")
+    plt.xlabel("Maximum Daily 8 Hour Ozone (ppb)", family="sans-serif")
+    plt.ylabel("Number of Days in Sample", family="sans=serif")
+    plt.grid(True)
+    plt.show()
+    output_path_wname = str(output_path) + str(output_prefix) + str(unit) + str(graph_id)
+    fig.savefig(output_path_wname, format="pdf")
+    return(True)
+
+def time_separator(df, time_unit):
+    grouped = df.groupby(by=time_unit, as_index=True, sort=True, group_keys=True)
+    print(grouped)
+    print(len(grouped))
+    print(grouped.max())
     return(True)
 
 def histo_builder_deprecated(df, x, y):
@@ -81,14 +89,13 @@ def histo_builder_deprecated(df, x, y):
 
     return(hist)
 
-def cluster_plotter(df):
+def cluster_byclusterplotter(df):
     i = 0
     while i < 16:
         cluster = df.loc[df['cluster'] == i]
-        graph = histo_builder(cluster, "maximum daily 8 hour ozone (ppb)", "number of days")
+        graph = histo_builder(cluster, "Cluster", i)
         plt.savefig("foo.pdf")
         i = i + 1
-    
     return(True)
 
 def site_plotter(df):
@@ -98,7 +105,7 @@ def year_overyearplotter(df):
     i = 2010
     while i < 2020:
         year = df.loc[df['year'] == i]
-        histo_builder(year, "maximum daily 8 hour ozone (ppb)", "number of days")
+        histo_builder(year, "Year", i, True, global_output_path, "year_over_year")
         i = i + 1
     return(True)
 
@@ -106,7 +113,7 @@ def month_bymonthplotter(df):
     i = 6
     while i < 10:
         month = df.loc[df['month'] == i]
-        histo_builder(month, "maximum daily 8 hour ozone (ppb)", "number of days")
+        histo_builder(month, "Month", i)
         i = i + 1
     return(True)
 
