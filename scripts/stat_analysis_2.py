0838794cb56d5fdd225bbfb4a6e16ca3c12ad2b1
pyromakesmusic <tayo.castro@gmail.com>
2022-10-24 22:49:19 -0500

    added some osmnx stuff

--- scripts/stat_analysis_2.py
+++ scripts/stat_analysis_2.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
+import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib as mpl
 
 def main():
+    houston = ox.gdf_from_place("Houston, Texas")
+    fig, ax = plt.subplots()
+    houston.plot(ax=ax)
     pass
 
 main()
