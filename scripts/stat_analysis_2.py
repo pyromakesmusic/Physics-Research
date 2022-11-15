f64edee608677d661edb4f75c89f58a37854e60a
pyromakesmusic <tayo.castro@gmail.com>
2022-11-15 10:30:41 -0600

    got houston mapped

--- scripts/stat_analysis_2.py
+++ scripts/stat_analysis_2.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-# import osmnx as ox
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import shapefile
 import matplotlib.animation as animation
 import matplotlib as mpl
+
+
 
 """
 CONFIG
@@ -20,28 +20,17 @@ pd.set_option('display.max_colwidth', None)
 
 mpl.rcParams['figure.dpi'] = 90
 
-
-
-"""
-def ox_map():
-    houston = ox.graph_from_place("Houston, Texas")
-    fig, ax = ox.plot_graph(houston, show=False, close=False)
-    coordinates = coords()
-    print(coordinates)
-    plt.plot(coordinates.Latitude, coordinates.Longitude)
-    plt.show()
-"""
-
 def map():
-    with open(r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\scripts\file_inputs\COH_ADMINISTRATIVE_BOUNDARY_-_MIL\COH_ADMINISTRATIVE_BOUNDARY_-_MIL.csv") as houston_shape:
-#        fig, ax = plt.subplots(111)
-        houston = pd.read_csv(houston_shape)
-        print(houston)
-#        houston.plot()
-#        plt.show()
-#        plt.draw()
-#        site_coordinates = coords()
-#        plt.plot(site_coordinates.Latitude, site_coordinates.Longitude)
+    houston_file = (r"D:\#PERSONAL\#STEDWARDS\#Summer2022Research\scripts\file_inputs\COH_ADMINISTRATIVE_BOUNDARY_-_MIL\COH_ADMINISTRATIVE_BOUNDARY_-_MIL.shp")
+    houston = gpd.read_file(houston_file)
+    houston.plot()
+    plt.show()
+
+
+def basemap():
+    plt.title("Houston/Galveston Bay Area")
+    plt.show()
+    return
 
 def coords():
     with open(r"file_inputs\site_coordinates.txt") as coords:
@@ -54,13 +43,14 @@ def coords():
         coords_output_df = pd.concat(coords_series_list, axis=1).T
         coords_output_df.columns = coords_output_df.iloc[0]
         coords_output_df = coords_output_df[1:]
+
         return coords_output_df
 
 def augmented_file():
     aug = open(r"file_inputs\SOM_cluster_ozone_augmentedv3.csv")
     aug_df = pd.read_csv(aug)
     print(aug_df)
-    stacked_bars(aug_df)
+    #stacked_bars(aug_df)
     aug.close()
 
 def stacked_bars(df):
@@ -68,7 +58,10 @@ def stacked_bars(df):
     plt.show()
     return
 def main():
+    #augmented_file()
+    #basemap()
     map()
-#    augmented_file()
+    site_coords = coords()
+    print(site_coords)
 
 main()
