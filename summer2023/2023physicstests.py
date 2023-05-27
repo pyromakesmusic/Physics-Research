e60af40b4bf0878675821127442ce69f4991fd96
pyromakesmusic <pyromakesmusic@gmail.com>
2023-05-27 03:55:57 -0500

    think i successfully read in the data into dataframe

--- summer2023/2023physicstests.py
+++ summer2023/2023physicstests.py
@@ -10,13 +10,8 @@ def main():
         print(config.readline().rstrip())
         data_path = config.readline().rstrip()
 
-    with open(data_path) as no2_file:
-        linelists = []
-
-        for line in no2_file.readlines():
-            linelists.append(pd.Series(line))
-        no2_df = pd.DataFrame(data=linelists)
-        print(no2_df)
+    no2_df = pd.read_csv(data_path)
+    print(no2_df)
 
 
 if __name__ == "__main__":
