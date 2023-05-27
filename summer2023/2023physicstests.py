ab68da85b4c01307896378ab015d16cababfe296
pyromakesmusic <pyromakesmusic@gmail.com>
2023-05-27 03:24:57 -0500

    need to format lines without commas, check if we can use the read_csv method

--- summer2023/2023physicstests.py
+++ summer2023/2023physicstests.py
@@ -11,17 +11,12 @@ def main():
         data_path = config.readline().rstrip()
 
     with open(data_path) as no2_file:
-        for i in range(47):
-            print(no2_file.readline().rstrip())
-
-        print("=========== \n BREAK BREAK BREAK BREAK BREAK \n ============")
         linelists = []
 
         for line in no2_file.readlines():
             linelists.append(pd.Series(line))
         no2_df = pd.DataFrame(data=linelists)
         print(no2_df)
-        no2_df.columns = no2_df[0]
 
 
 if __name__ == "__main__":
