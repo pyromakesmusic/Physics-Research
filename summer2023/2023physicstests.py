4b822a4925c970843d8c559833d12882cf05dc05
pyromakesmusic <pyromakesmusic@gmail.com>
2023-06-10 03:25:44 -0500

    updated to label site 25, need 18

--- summer2023/2023physicstests.py
+++ summer2023/2023physicstests.py
@@ -12,23 +12,22 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 def main():
-    data_path = "placeholder"
+    site25_data_path = "placeholder"
     with open("config.txt") as config:
         print(config.readline().rstrip())
-        data_path = config.readline().rstrip()
+        site25_data_path = config.readline().rstrip()
 
-    no2_df = pd.read_csv(data_path)
-    no2_day_df = no2_df[no2_df[' Datetime'] > 7942]
-    # print(no2_df)
-    # print(no2_df.shape)
-    # print(no2_df.columns)
-    print(no2_df[' Datetime'])
-    plt.scatter(' Solar_zenith', ' NO2_total_vertical_column', data=no2_df, s=2)
+    site25_no2_df = pd.read_csv(site25_data_path)
+    no2_day_df = site25_no2_df[site25_no2_df[' Datetime'] > 7942]
+    # print(site25_no2_df)
+    # print(site25_no2_df.shape)
+    # print(site25_no2_df.columns)
+    plt.scatter(' Solar_zenith', ' NO2_total_vertical_column', data=site25_no2_df, s=2)
     plt.xlabel("Solar_zenith")
     plt.ylabel("Total vertical column NO2 in moles/m^2")
     plt.yticks()
     plt.show()
-    print(no2_df.columns)
+    print(site25_no2_df.columns)
 
 
 if __name__ == "__main__":
