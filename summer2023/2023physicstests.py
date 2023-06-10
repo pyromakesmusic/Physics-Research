a82b8707c9aa9a261c9c9c31b6a5892639567fb2
pyromakesmusic <pyromakesmusic@gmail.com>
2023-06-10 06:15:47 -0500

    cleaning up the code

--- summer2023/2023physicstests.py
+++ summer2023/2023physicstests.py
@@ -16,18 +16,15 @@ def main():
     with open("config.txt") as config:
         print(config.readline().rstrip())
         site25_data_path = config.readline().rstrip()
+        print(config.readline().rstrip())
+        site188_data_path = config.readline().rstrip()
 
     site25_no2_df = pd.read_csv(site25_data_path)
-    no2_day_df = site25_no2_df[site25_no2_df[' Datetime'] > 7942]
-    # print(site25_no2_df)
-    # print(site25_no2_df.shape)
-    # print(site25_no2_df.columns)
-    plt.scatter(' Solar_zenith', ' NO2_total_vertical_column', data=site25_no2_df, s=2)
-    plt.xlabel("Solar_zenith")
-    plt.ylabel("Total vertical column NO2 in moles/m^2")
-    plt.yticks()
+    site188_no2_df = pd.read_csv(site188_data_path)
+
+    # Here go the commands, may want to modularize the code somewhat
+
     plt.show()
-    print(site25_no2_df.columns)
 
 
 if __name__ == "__main__":
