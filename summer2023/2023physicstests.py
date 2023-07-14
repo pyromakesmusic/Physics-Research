ae1276e7e46db519e330b6391944b118d08c4710
pyromakesmusic <pyromakesmusic@gmail.com>
2023-07-13 23:19:07 -0500

    repaired and restored to starting git history configuration

--- summer2023/2023physicstests.py
+++ summer2023/2023physicstests.py
@@ -20,12 +20,13 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 def main():
-    site25_data_path = "placeholder"
     with open("config.txt") as config:
         print(config.readline().rstrip())
         site25_data_path = config.readline().rstrip()
         print(config.readline().rstrip())
         site188_data_path = config.readline().rstrip()
+        print(config.readline().rstrip())
+        pgn25_data_path = config.readline().rstrip()
 
     site25_no2_df = pd.read_csv(site25_data_path)
     site188_no2_df = pd.read_csv(site188_data_path)
